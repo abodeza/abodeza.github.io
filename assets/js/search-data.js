52fb80165a8d210f76537077ed9813b948dcbf34
abodeza <145087395+abodeza@users.noreply.github.com>
2025-05-22 22:14:12 +0000

    Deploying to gh-pages from @ abodeza/abodeza.github.io@c678496a34c2ad52a440dd8f162c632e3015ecc2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "My CV rendered from standard json. A PDF copy is also available by clicking on the PDF icon to the right.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
