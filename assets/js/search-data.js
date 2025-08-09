cabaa3b74c17a16b429c2d9762db193187f5ff66
abodeza <145087395+abodeza@users.noreply.github.com>
2025-08-09 20:44:32 +0000

    Deploying to gh-pages from @ abodeza/abodeza.github.io@e6b9f49e7d4a1da521d875c01309444c640c7e19 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,9 +37,9 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/certificates/";
           },
-        },{id: "post-masking-diseases-on-plant-leaves",
+        },{id: "post-masking-plant-diseases-opencv",
         
-          title: 'Masking Diseases on Plant Leaves <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
+          title: 'Masking Plant Diseases [OpenCV] <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
         
         description: "",
         section: "Posts",
